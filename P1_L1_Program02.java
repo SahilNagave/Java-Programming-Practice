460d1f2729ff2bbc7a3a8a84e9646f511099981f
SahilNagave <sahilnagave7@gmail.com>
2026-09-03 19:43:15 +0530

    Add files via upload

---
+++ P1_L1_Program02.java
@@ -0,0 +1,13 @@
+// Print personal details
+
+public class P1_L1_Program02 {
+    public static void main(String [] args) {
+
+        System.out.println("Name    : Sahil Nagave");
+        System.out.println("Age     : 20");
+        System.out.println("College : APCOER");
+        System.out.println("Branch  : Information Technology");
+
+    }
+
+}
