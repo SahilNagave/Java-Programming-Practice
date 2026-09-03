65757c79b29a2b17941f3fbef9bea07d79ced530
SahilNagave <sahilnagave7@gmail.com>
2026-09-03 19:07:50 +0530

    Add files via upload

---
+++ P1_L1_Program01.java
@@ -0,0 +1,10 @@
+// Write a Java program to print: Hello, World!
+
+public class P1_L1_Program01 {
+    public static void main(String [] args){
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
