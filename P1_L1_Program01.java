e797e22dc7f798cf76807fe4b19eadbca8e81f2f
SahilNagave <sahilnagave7@gmail.com>
2026-09-03 18:57:30 +0530

    Delete P1_L1_Program01.java

--- P1_L1_Program01.java
+++
@@ -1,9 +0,0 @@
-// Write a Java program to print: Hello, World!
-public class P1_L1_Program01 {
-    public static void main(String [] args){
-
-        System.out.println("Hello, World!");
-
-    }
-
-}
