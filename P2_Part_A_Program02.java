215780501c396e8d232da51012d966ba9a71e5de
SahilNagave <sahilnagave7@gmail.com>
2026-09-12 21:56:00 +0530

    Add files via upload

---
+++ P2_Part_A_Program02.java
@@ -0,0 +1,23 @@
+// Take an integer and determine whether it is even or odd.
+
+import java.util.Scanner;
+
+public class P2_Part_A_Program02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+
+        if(number % 2 == 0) {
+            System.out.println(number + " is an EVEN number");
+        }
+
+        else {
+            System.out.println(number + " is an ODD number");
+        }
+        
+        sc.close();
+    }
+}
