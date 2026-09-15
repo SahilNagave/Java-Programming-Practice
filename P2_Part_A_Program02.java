ba6f5712a2181b5c5f90ffab59c11d3175ba1bba
SahilNagave <sahilnagave7@gmail.com>
2026-09-15 09:48:39 +0530

    Add files via upload

---
+++ P2_Part_A_Program02.java
@@ -0,0 +1,34 @@
+// Take a number and print whether it is:
+
+// Positive
+// Negative
+// Zero
+
+import java.util.Scanner;
+
+public class P2_Part_A_Program01 {
+    public static void main(String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+
+        if(number == 0) {
+            System.out.println("Input is a ZERO");
+        }
+
+        else if(number > 0) {
+            System.out.println(number + " is a POSITIVE NUMBER");
+        }
+
+        else {
+            System.out.println(number + " is a NEGATIVE NUMBER");
+
+        }
+
+        sc.close();
+
+    }
+
+}
